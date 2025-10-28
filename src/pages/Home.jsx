f91b1151853bb8618211f14615d4faa3fba820af
Taha-Yassine-Hadded <yessinhadded99@gmail.com>
2025-10-28 12:06:19 +0100

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="flex-grow bg-white flex items-center justify-center">
+      <div className="container mx-auto px-4 py-6">
+        <h1 className="text-3xl font-bold text-center">Welcome ShopHUB</h1>
+        <p className="text-center text-gray-600 mt-4">
+          Discover the best products!
+        </p>
+      </div>
+    </main>
+  );
+}
